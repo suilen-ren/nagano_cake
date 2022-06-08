d1ff4b35b4cfecf38df3dd88e6430f8cd7bd0cc6
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-06-08 13:11:17 +0000

    息抜きにレイアウトいじってます

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+admin=Admin.new(
+  email: "hogehoge@hogehoge",
+  password: "ngo0128ren"
+)
+admin.save!

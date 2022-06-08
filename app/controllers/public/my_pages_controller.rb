1b11ed8750ffb4db73ec9ef682ad84c602c602eb
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-06-08 08:05:50 +0000

    in progress -withdraw

--- app/controllers/public/my_pages_controller.rb
+++ app/controllers/public/my_pages_controller.rb
@@ -7,11 +7,27 @@ class Public::MyPagesController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to my_pages_path
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def leave
+    @customer = current_customer
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:email,:last_name,:last_name_kana,:first_name,:first_name_kana,:postal_code,:telephone_number,:address,:is_active)
+  end
+
 end

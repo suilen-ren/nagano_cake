18f124902c583a00710240c3cafc4be733132674
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-06-20 08:10:48 +0000

    Fixed--public/cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,10 +12,6 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.where(customer_id: current_customer.id)
   end
-  
-  def subtotal
-  end
-
 
   def update
     @cart_item = CartItem.find(params[:id])
@@ -39,15 +35,29 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    if @cart_item.save
-      flash[:notice] = "商品を買い物かごに追加しました"
-      redirect_to items_path
-    else
-      flash[:alert] = "個数を選択してください"
-      redirect_to item_path(@cart_item.item_id)
-    end
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      cart_item_check = []
+      #カート内に同じ商品が被っていたときの処理
+      current_customer.cart_items.each do |cart_item|
+        cart_item_check << cart_item.item_id
+      end
+
+      if @cart_item.quantity == nil #個数選択されてない場合を弾く処理
+        flash[:alert] = "個数を選択してください"
+        redirect_to item_path(@cart_item.item_id)
+      elsif cart_item_check.any? {|n| n == @cart_item.item_id }
+        @fix_cart_item = CartItem.find_by(item_id: @cart_item.item_id)
+        fix_quantity = @fix_cart_item.quantity + @cart_item.quantity
+        @fix_cart_item.update(quantity: fix_quantity.to_i)
+        flash[:notice] = "個数を追加しました"
+        redirect_to items_path
+      #ここまで
+      else
+        @cart_item.save
+        flash[:notice] = "商品を買い物かごに追加しました"
+        redirect_to items_path
+      end
   end
 
   private
